fa40a4cd51fefed8922e4adf924f04a186e8db3b
Eunice2000 <adediraneunice20@gmail.com>
2023-04-24 13:58:35 +0100

    client

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const url = 'ec2-44-201-194-87.compute-1.amazonaws.com';
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
